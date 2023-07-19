9e2d268db1db13bcdeb593aab431c0ab0ecef66a
Chaudhary Mohit Singh <108528053+ChMohitSingh@users.noreply.github.com>
2023-07-19 14:17:59 +0530

    Add files via upload

---
+++ calculator/index.js
@@ -0,0 +1,23 @@
+let inputE=document.querySelector('input')
+let btn=document.querySelectorAll('button')
+for(let button of btn){
+    button.addEventListener('click',(e)=>{
+       let text= e.target.innerText
+       if(text=='C'){
+        inputE.value=''
+       }
+       else if(text==='='){
+        try{
+            inputE.value=eval(inputE.value)
+        }
+        catch(e){
+            inputE.value=('')
+        }
+       }
+       else{
+        inputE.value=inputE.value+text
+        }
+    //    console.log(text)
+
+    })
+}
